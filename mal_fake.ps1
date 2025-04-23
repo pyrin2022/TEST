8e475d87bb2d94bdca1e148d5faa8b8a447f33b3
Pyrin2022 <118145791+pyrin2022@users.noreply.github.com>
2025-04-23 21:22:16 +0900

    Update mal_fake.ps1

--- mal_fake.ps1
+++ mal_fake.ps1
@@ -1,8 +1,1 @@
-$User = $env:USERNAME
-$IP = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias 'Ethernet*','Wi-Fi*' -ErrorAction SilentlyContinue | Where-Object {$_.IPAddress -ne "127.0.0.1"} | Select-Object -First 1).IPAddress
-$Time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-$Log = "User: $User`nIP: $IP`nTime: $Time"
-
-$Path = "C:\Temp\log.txt"
-New-Item -Path "C:\Temp" -ItemType Directory -Force | Out-Null
-$Log | Out-File -FilePath $Path -Encoding UTF8
+테스트
